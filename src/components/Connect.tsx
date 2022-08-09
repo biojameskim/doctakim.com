2c6a56f86d4ad71da30a2611f081fada5f45acaa
James Kim <jjk297@cornell.edu>
2022-08-09 01:31:10 -0700

    Please enter the commit message for your changes. Lines starting

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,7 +1,7 @@
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { FiMail } from 'react-icons/fi';
 import { AiOutlinePhone } from 'react-icons/ai';
-import { Box, Image, VStack, HStack, Button, Text, useColorModeValue } from '@chakra-ui/react';
+import { Box, Image, VStack, HStack, Button, Text} from '@chakra-ui/react';
 import ProfilePic from "../images/pictures/avatar.jpeg"
 import { Helmet } from 'react-helmet';
 
