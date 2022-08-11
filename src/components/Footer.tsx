3d1db88affd467337c75fcd6bdfd5770b1815c13
James Kim <jjk297@cornell.edu>
2022-08-11 15:27:13 -0700

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,34 +11,36 @@ import {
   import SocialButton from './SocialButton';
 
   
-  export default function Footer() {
-    return (
-      <Box
-        bg={useColorModeValue('gray.100', 'gray.900')}
-        color={useColorModeValue('gray.700', 'gray.200')}
-      >
-        <Container
-          as={Stack}
-          maxW={'6xl'}
-          py={4}
-          direction={{ base: 'column', md: 'row' }}
-          spacing={4}
-          justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}>
-          <Text>© 2022 James Kim</Text>
-          <Stack direction={'row'} spacing={6} px={20}>
-          <Link to="/home">
-              <SocialButton label={'Home'}>
-                <AiOutlineHome />
-              </SocialButton>
-            </Link>
-            <Link to="/">
-              <SocialButton label={'BacktoStart'}>
-                <VscDebugRestart />
-              </SocialButton>
-            </Link>
-          </Stack>
-        </Container>
-      </Box>
-    );
-  }
+const Footer = () => {
+  return (
+    <Box
+      bg={useColorModeValue('gray.100', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}
+    >
+      <Container
+        as={Stack}
+        maxW={'6xl'}
+        py={4}
+        direction={{ base: 'column', md: 'row' }}
+        spacing={4}
+        justify={{ base: 'center', md: 'space-between' }}
+        align={{ base: 'center', md: 'center' }}>
+        <Text>© 2022 James Kim</Text>
+        <Stack direction={'row'} spacing={6} px={20}>
+        <Link to="/home">
+            <SocialButton label={'Home'}>
+              <AiOutlineHome />
+            </SocialButton>
+          </Link>
+          <Link to="/">
+            <SocialButton label={'BacktoStart'}>
+              <VscDebugRestart />
+            </SocialButton>
+          </Link>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
+
+export default Footer
