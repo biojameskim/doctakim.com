d9312e3189e9b2940e10922b1ff741c22082c9b9
James Kim <biojameskim2002@gmail.com>
2022-10-25 19:43:48 -0400

    padding adjusted for BlogImage

--- src/components/BlogImage.tsx
+++ src/components/BlogImage.tsx
@@ -2,7 +2,7 @@ import { Box, Image, Text } from "@chakra-ui/react"
 
 const BlogImage = ({ src, alt, caption, caption2 }: { src: string, alt: string, caption?: string, caption2?: string }) => {
   return (
-    <Box paddingY='5vh' maxWidth={'600px'} alignSelf='center'>
+    <Box pt='5vh' pb='2vh' maxWidth={'600px'} alignSelf='center'>
       <Image src={src} alt={alt} borderRadius='xl' />
       <Box height='0.5rem'></Box>
       <Text as='i' fontSize={{ base: "md", md: "lg" }} paddingLeft='1.2rem'>{caption}</Text>
