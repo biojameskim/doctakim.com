14258f5f09348f76afa6dd212a9ffa998f3e4710
James Kim <biojameskim2002@gmail.com>
2022-12-16 22:12:46 -0800

    Update About page
    - Update font size and style

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,7 +15,7 @@ const About = () => {
         <title>doctakim | About</title>
       </Helmet>
       <VStack>
-        <Text fontSize='4rem' fontWeight={'medium'} pt={'6vh'} pb={'1vh'} >
+        <Text className="page-title" fontSize='4rem' fontWeight={'medium'} pt={'6vh'} pb={{ base: '2vh', md: '4vh' }} >
           About
         </Text>
         <Flex alignItems={'center'} w={{ base: '100%', md: '90%' }} direction={{ base: 'column', md: 'row' }}>
@@ -23,23 +23,25 @@ const About = () => {
             <Carousel />
           </Box>
 
-          <Box as={Stack} w={{ base: '90%', md: '52%' }} spacing='1.5rem' p={{ base: '0', md: '5' }} pt={{ base: '7' }}>
-            <Text fontSize='2.3rem'>Hello! 👋 </Text>
-            <Text fontSize='1.65rem'>
+          <Box as={Stack} w={{ base: '90%', md: '46%' }} spacing='1.5rem' p={{ base: '3', md: '0' }} pt={{ base: '7' }}>
+            <Text fontSize='1.65rem'>Hello! 👋 </Text>
+            <Text fontSize='1.2rem'>
               I'm currently a student at Cornell University studying Computer Science with an intended minor in Digital Culture and Production.
             </Text>
-            <Text fontSize='1.65rem'>
+            <Text fontSize='1.2rem'>
               Outside of CS, I am an avid listener of Korean rap and hiphop music and love to play tennis.
             </Text>
-            <Text fontSize='1.65rem'>
+            <Text fontSize='1.2rem'>
               I'm open to any opportunities to learn more about software development and computer science in general!
             </Text>
-            <Text className="about-section" fontSize='1.65rem'>
-              Please feel free to reach out!
-            </Text>
-            <Link to="/connect" className="about-section">
-              <Button fontSize='1.65rem' size='lg'>Connect</Button>
-            </Link>
+            <VStack pt="1rem" spacing="1.5rem">
+              <Text className="about-section" fontSize='1.2rem'>
+                Please feel free to reach out!
+              </Text>
+              <Link to="/connect" className="about-section">
+                <Button className="custom-font-button" fontSize='1.65rem' size='lg'>Connect</Button>
+              </Link>
+            </VStack>
           </Box>
         </Flex>
         <Box pb='6vh'></Box>
