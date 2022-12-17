3e0554d9173ae01630387e1b4b3ca4e96e40d8a9
James Kim <biojameskim2002@gmail.com>
2022-12-16 23:26:21 -0800

    Update Experience to Employment

---
+++ src/components/EmploymentCard.tsx
@@ -0,0 +1,54 @@
+import { Text, Box, useColorModeValue, Flex, Stack, Image, SimpleGrid, GridItem, useBreakpointValue, Hide, Show } from "@chakra-ui/react"
+
+const EmploymentCard = ({ website, title, role, image, alt, date, description }: { website: string, title: string, role: string, image: string, alt: string, date: string, description: string }) => {
+  const colSpan = useBreakpointValue({ base: 2, md: 1 })
+  return (
+    <Box className="employment-card"
+      as="a"
+      target={"_blank"}
+      href={website}
+      p={'0.5rem'}
+      pt={{ base: '6', md: '0.5rem' }}
+      margin="auto"
+      mt={10}
+      maxW={'80%'}
+      w={'full'}
+      shadow='lg'
+      borderWidth='1px'
+      position="relative"
+      rounded='lg'
+      bg={useColorModeValue('gray.100', 'gray.900')}>
+      <Flex justify='left' direction={{ base: 'column', md: 'row' }}>
+
+        <Box>
+          <Image
+            borderRadius='2xl'
+            maxWidth={'150px'}
+            bg={useColorModeValue('gray.300', 'gray.700')}
+            p='2'
+            src={image}
+            alt={alt}
+          />
+        </Box>
+
+        <Stack pl='5' spacing='1'>
+          <Flex w='100%' justify='flex-start' align='center' direction={{ base: 'column', md: 'row' }}>
+            <Text fontSize='2rem' fontWeight='bold' pt={{ base: '1rem', md: '0' }} >{title}</Text>
+            <Hide below='md'>
+              <Text fontSize='2rem' px='6'>|</Text>
+            </Hide>
+            <Text fontSize='1.5rem'>{role}</Text>
+          </Flex>
+          <Text fontSize='1.2rem' color='gray.500' pb='0.5' textAlign={{ base: 'center', md: 'left' }}>
+            {date}
+          </Text>
+          <Text fontSize='1.2rem' w='95%'>
+            {description}
+          </Text>
+        </Stack>
+      </Flex>
+    </Box>
+  )
+}
+
+export default EmploymentCard
---
+++ src/pages/Employment.tsx
@@ -0,0 +1,54 @@
+import { VStack, Text, Box } from "@chakra-ui/react"
+import { Helmet } from 'react-helmet';
+import EmploymentCard from '../components/EmploymentCard'
+import { useEffect } from "react";
+
+const Employment = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  return (
+    <div>
+      <Helmet>
+        <title>doctakim | Employment</title>
+      </Helmet>
+      <VStack
+        spacing='3vh'
+      >
+        <Text className="page-title" fontSize='4rem' fontWeight={'medium'} pt={'6vh'} pb={{ base: '2vh', md: '4vh' }} >
+          Employment
+        </Text>
+
+        <EmploymentCard
+          website='https://cornellh4i.org/'
+          title='hack4impact'
+          role='Full Stack Software Engineer'
+          image={"images/logos/hack4impact.jpeg"}
+          alt='hack4impact Logo'
+          date='09/2022 - Present'
+          description='Building a platform for the homeless population of 
+          Tompkins County to browse for available and affordable housing using 
+          MongoDB, Express.js, React, and Node.js'
+        />
+
+        <EmploymentCard
+          website='https://wazzle.app'
+          title='Wazzle'
+          role='Mobile Development Intern'
+          image={"images/logos/wazzle.jpeg"}
+          alt='Wazzle Logo'
+          date='04/2022 - 08/2022'
+          description='Implemented new features and design changes for an iOS 
+          application that allows for more meaningful contact management for over 5000 users
+          using Swift and SwiftUI'
+        />
+
+      </VStack>
+      <Box pb='6vh'></Box>
+    </div>
+
+  )
+}
+
+export default Employment
