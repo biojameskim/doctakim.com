0a5a78b9698a2f37e84e9d3963d822c7367acaf8
James Kim <biojameskim2002@gmail.com>
2022-12-26 19:24:48 -0800

    Updated BlogImage to take maxWidth as a parameter

--- src/components/BlogImage.tsx
+++ src/components/BlogImage.tsx
@@ -1,8 +1,8 @@
 import { Box, Image, Text } from "@chakra-ui/react"
 
-const BlogImage = ({ src, alt, caption, caption2 }: { src: string, alt: string, caption?: string, caption2?: string }) => {
+const BlogImage = ({ src, alt, caption, caption2, maxW = '600px' }: { src: string, alt: string, caption?: string, caption2?: string, maxW?: string }) => {
   return (
-    <Box pt='5vh' pb='2vh' maxWidth={'600px'} alignSelf='center'>
+    <Box pt='5vh' pb='2vh' maxWidth={maxW} alignSelf='center'>
       <Image src={src} alt={alt} borderRadius='xl' />
       <Box height='0.5rem'></Box>
       <Text as='i' fontSize={{ base: "md", md: "lg" }} paddingLeft='1.2rem'>{caption}</Text>
