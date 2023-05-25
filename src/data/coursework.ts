bfdd8b3818fadfc4b6bf11e2ec5ada98e8761203
James Kim <biojameskim2002@gmail.com>
2023-05-25 11:19:50 -0700

    Refactor skills and courses page

---
+++ src/data/coursework.ts
@@ -0,0 +1,48 @@
+export type Coursework = {
+  courseTitle: string,
+  courseNumber: string,
+  affiliation: string
+}
+
+export const coursework: Coursework[] = [
+  {
+    courseTitle: "Analysis of Algorithms",
+    courseNumber: "CS 4820",
+    affiliation: "Associated with Cornell University"
+  },
+  {
+    courseTitle: "Data Structures and Functional Programming",
+    courseNumber: "CS 3110",
+    affiliation: "Associated with Cornell University"
+  },
+  {
+    courseTitle: "Discrete Structures",
+    courseNumber: "CS 2800",
+    affiliation: "Associated with Cornell University"
+  },
+  {
+    courseTitle: "Object-Oriented Programming and Data Structures",
+    courseNumber: "CS 2110",
+    affiliation: "Associated with Cornell University"
+  },
+  {
+    courseTitle: "Linear Algebra",
+    courseNumber: "MATH 2210",
+    affiliation: "Associated with Cornell University"
+  },
+  {
+    courseTitle: "Introduction to Backend Development",
+    courseNumber: "CS 1998",
+    affiliation: "Associated with Cornell University"
+  },
+  {
+    courseTitle: "Introduction to Computing using Python",
+    courseNumber: "CS 1110",
+    affiliation: "Associated with Cornell University"
+  },
+  {
+    courseTitle: "Design and Programming for the Web",
+    courseNumber: "INFO 1300",
+    affiliation: "Associated with Cornell University"
+  },
+]
--- src/pages/Coursework.tsx
+++ src/pages/Coursework.tsx
@@ -1,21 +1,27 @@
-import { Box, Flex, Text, Stack, List, ListItem, ListIcon } from "@chakra-ui/react"
+import { Box, Flex, Text, Stack, List, ListItem, ListIcon, Grid } from "@chakra-ui/react"
 import { BsCircle } from 'react-icons/bs'
 import CourseworkCard from "../components/CourseworkCard"
 import { useEffect } from "react";
 
+import { coursework } from "../data/coursework";
+
 const Coursework = () => {
+  // Makes an array from [0, ..., N-1] where N is how many coursework data we have
+  const numbers = Array.from(Array(coursework.length).keys());
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
 
   return (
     <div>
-      <Flex w={{ base: '100%', md: '80%' }} mt='1.5rem' direction={{ base: 'column', md: 'row' }}>
-        <Stack w={{ base: '90%', md: '50%' }} spacing='10' mt='0' align='center'>
-          <Text fontSize='1.5rem' fontWeight='bold' mb='0rem' mt={{ base: '2rem', md: '0' }}>Technical Skills</Text>
-          <Stack spacing='2rem'>
-            <Stack spacing='4' align='left'>
-              <Text align='left' fontSize='1.2rem' fontWeight='bold'>Languages</Text>
+      <Flex w={{ base: '100%', md: '100%' }} mt='2rem' direction={{ base: 'column', md: 'column' }}>
+        <Flex direction='column' w={{ base: '90%', md: '100%' }} mt='0' align='center' mb='5vh'>
+          <Text fontSize='1.5rem' fontWeight='bold' mb='3rem' mt={{ base: '2rem', md: '0' }}>Technical Skills</Text>
+          <Flex direction={{ base: 'column', md: 'row' }} w='80%' justifyContent={'space-evenly'} alignContent={'flex-start'}>
+
+            <Flex direction='column' mb='2rem'>
+              <Text align='left' fontSize='1.2rem' fontWeight='bold' mb='2vh'>Languages</Text>
               <List spacing='0.5rem'>
                 <ListItem textAlign='left'>
                   <ListIcon as={BsCircle} color='green.500' />
@@ -26,10 +32,10 @@ const Coursework = () => {
                   TypeScript, JavaScript, Swift, SQL, Bash
                 </ListItem>
               </List>
-            </Stack>
+            </Flex>
 
-            <Stack spacing='4' align='left'>
-              <Text align='left' fontSize='1.2rem' fontWeight='bold'>Frameworks & Libraries</Text>
+            <Flex direction='column' mb='2rem'>
+              <Text align='left' fontSize='1.2rem' fontWeight='bold' mb='2vh'>Frameworks & Libraries</Text>
               <List spacing='0.5rem'>
                 <ListItem textAlign='left'>
                   <ListIcon as={BsCircle} color='green.500' />
@@ -40,57 +46,37 @@ const Coursework = () => {
                   Flask, Pandas, NumPy, SQLite, SQLAlchemy
                 </ListItem>
               </List>
-            </Stack>
+            </Flex>
 
-            <Stack spacing='4'>
-              <Text align='left' fontSize='1.2rem' fontWeight='bold'>Tools</Text>
+            <Flex direction='column' mb='2rem'>
+              <Text align='left' fontSize='1.2rem' fontWeight='bold' mb='2vh'>Tools</Text>
               <List spacing='0.5rem'>
                 <ListItem textAlign='left'>
                   <ListIcon as={BsCircle} color='green.500' />
                   Git, Docker, Postman, Linux, XCode
                 </ListItem>
+                <ListItem textAlign='left'>
+                  <ListIcon as={BsCircle} color='white' />
+                  {/* Dummy ListItem because it doesn't align correctly otherwise */}
+                </ListItem>
               </List>
-            </Stack>
-          </Stack>
-        </Stack>
+            </Flex>
 
-        <Box w={{ base: '100%', md: '50%' }} mt={{ base: '4rem', md: '0' }}>
-          <Text fontSize='1.5rem' fontWeight='bold' mb='0rem'>Coursework</Text>
-          <CourseworkCard
-            courseTitle="Data Structures and Functional Programming"
-            courseNumber="CS 3110"
-            affiliation="Associated with Cornell University"
-          />
-          <CourseworkCard
-            courseTitle="Discrete Structures"
-            courseNumber="CS 2800"
-            affiliation="Associated with Cornell University"
-          />
-          <CourseworkCard
-            courseTitle="Object-Oriented Programming and Data Structures"
-            courseNumber="CS 2110"
-            affiliation="Associated with Cornell University"
-          />
-          <CourseworkCard
-            courseTitle="Linear Algebra"
-            courseNumber="MATH 2210"
-            affiliation="Associated with Cornell University"
-          />
-          <CourseworkCard
-            courseTitle="Introduction to Backend Development"
-            courseNumber="CS 1998"
-            affiliation="Associated with Cornell University"
-          />
-          <CourseworkCard
-            courseTitle="Introduction to Computing using Python"
-            courseNumber="CS 1110"
-            affiliation="Associated with Cornell University"
-          />
-          <CourseworkCard
-            courseTitle="Introductory Design and Programming for the Web"
-            courseNumber="INFO 1300"
-            affiliation="Associated with Cornell University"
-          />
+          </Flex>
+        </Flex>
+
+        <Box w='full' mt={{ base: '4rem', md: '0' }}>
+          <Text fontSize='1.5rem' fontWeight='bold' mb='3rem'>Coursework</Text>
+          <Grid w={{ base: '95%', md: '70%' }} templateColumns={{ md: 'repeat(2, 1fr)' }} gap='6' mb='10vh'>
+            {numbers.map(n =>
+              <CourseworkCard
+                key={n}
+                courseTitle={coursework[n].courseTitle}
+                courseNumber={coursework[n].courseNumber}
+                affiliation={coursework[n].affiliation}
+              />
+            )}
+          </Grid>
         </Box>
       </Flex>
     </div>
