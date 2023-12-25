00ead6c86a4f948c9fd2717e411f7ac9bf70157e
James Kim <biojameskim2002@gmail.com>
2023-12-25 08:49:52 -0800

    Add Research section

--- src/pages/experience/Experience.tsx
+++ src/pages/experience/Experience.tsx
@@ -1,14 +1,23 @@
-import { Text, Box, Tabs, TabList, Tab, TabPanels, TabPanel, useColorModeValue } from "@chakra-ui/react"
-import Employment from "./Employment"
-// import Projects from "./Projects"
-import Coursework from "./Coursework"
-import { useEffect } from "react"
-import { Helmet } from 'react-helmet';
+import {
+  Text,
+  Box,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Employment from "./Employment";
+import Research from "./Research";
+// import Coursework from "./Coursework";
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
 
 const Experience = () => {
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div>
@@ -17,30 +26,44 @@ const Experience = () => {
       </Helmet>
 
       <Box>
-        <Text className="page-title" align='center' fontSize='4rem' fontWeight={'medium'} pt={'6vh'} pb={{ base: '2vh', md: '4vh' }} >
+        <Text
+          className="page-title"
+          align="center"
+          fontSize="4rem"
+          fontWeight={"medium"}
+          pt={"6vh"}
+          pb={{ base: "2vh", md: "4vh" }}
+        >
           Experience
         </Text>
-        <Tabs orientation='horizontal' variant='soft-rounded' colorScheme='green' align='center'>
+        <Tabs
+          orientation="horizontal"
+          variant="soft-rounded"
+          colorScheme="green"
+          align="center"
+        >
           <TabList>
-            <Tab color={useColorModeValue('gray.600', 'white')} >Employment</Tab>
-            {/* <Tab color={useColorModeValue('gray.600', 'white')}>Projects</Tab> */}
-            <Tab color={useColorModeValue('gray.600', 'white')}>Skills & Courses</Tab>
+            <Tab color={useColorModeValue("gray.600", "white")}>Employment</Tab>
+            <Tab color={useColorModeValue("gray.600", "white")}>Research</Tab>
+            {/* <Tab color={useColorModeValue("gray.600", "white")}>
+              Skills & Courses
+            </Tab> */}
           </TabList>
           <TabPanels>
             <TabPanel>
               <Employment />
             </TabPanel>
-            {/* <TabPanel>
-              <Projects />
-            </TabPanel> */}
             <TabPanel>
-              <Coursework />
+              <Research />
             </TabPanel>
+            {/* <TabPanel>
+              <Coursework />
+            </TabPanel> */}
           </TabPanels>
         </Tabs>
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
