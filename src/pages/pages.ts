25f41a78c07a9ee6ba0b2f07f0fb038d200d8ee9
James Kim <biojameskim2002@gmail.com>
2024-03-20 09:35:16 -0400

    Update pages

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -7,4 +7,3 @@ export { default as Blog } from './blog/Blog';
 export { default as Experience } from './experience/Experience';
 export { default as Hidden } from './hidden/Hidden';
 export { default as Birthdays } from './hidden/Birthdays';
-export { default as Jocelyn } from './hidden/Jocelyn';
