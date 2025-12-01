35ba783710778fb62a4e515d31c87e44bd367c1f
biojameskim <biojameskim2002@gmail.com>
2025-12-01 00:50:24 -0600

    chore: update Spotify music data

--- src/data/top_songs_artists.ts
+++ src/data/top_songs_artists.ts
@@ -1,5 +1,11 @@
 export const topSongs = [
     {
+        "title": "The Only One",
+        "artist": "SOYOU",
+        "cover": "https://i.scdn.co/image/ab67616d0000b273b76962f92ed440093f7e35d9",
+        "link": "https://open.spotify.com/track/7mk8b7hcnfOu4aZy2gCdWl"
+    },
+    {
         "title": "December 25th",
         "artist": "Charlie Puth",
         "cover": "https://i.scdn.co/image/ab67616d0000b2739a44bc42f5e541fe97f92823",
@@ -12,30 +18,29 @@ export const topSongs = [
         "link": "https://open.spotify.com/track/2ONgnWMIA0KRkyLX6sxkDj"
     },
     {
-        "title": "Reno (feat. Colde)",
-        "artist": "MIYEON, Colde",
-        "cover": "https://i.scdn.co/image/ab67616d0000b2730eb2c00ef777a800afe37a25",
-        "link": "https://open.spotify.com/track/4VRcawzVCLzsGGv9sk8CLA"
+        "title": "すきっ！ - 超ver",
+        "artist": "Cho Tokimeki Sendenbu",
+        "cover": "https://i.scdn.co/image/ab67616d0000b273f7e91b039f9948fc31e42c70",
+        "link": "https://open.spotify.com/track/0SxNslJ4zylzWBQTX4rBIe"
     },
     {
         "title": "Bathroom Floor",
         "artist": "Isabel van Gelder",
         "cover": "https://i.scdn.co/image/ab67616d0000b273d7debdd8a66b88e36739283c",
         "link": "https://open.spotify.com/track/3QFFda4IODSKbT839NW6BR"
-    },
-    {
-        "title": "plod",
-        "artist": "Gist",
-        "cover": "https://i.scdn.co/image/ab67616d0000b2731bad433e55b30335ae80c4c1",
-        "link": "https://open.spotify.com/track/6tL0g1RaykOO81xugkbGXb"
     }
 ];
 
 export const topArtists = [
     {
-        "name": "PRYVT",
-        "image": "https://i.scdn.co/image/ab6761610000e5eb012e40962788790bb7b60f75",
-        "link": "https://open.spotify.com/artist/2EoyTW14yqnbqmk90NjbLT"
+        "name": "Heize",
+        "image": "https://i.scdn.co/image/ab6761610000e5eb14df5a6a1377b0cd425a328c",
+        "link": "https://open.spotify.com/artist/5dCvSnVduaFleCnyy98JMo"
+    },
+    {
+        "name": "HAON",
+        "image": "https://i.scdn.co/image/ab6761610000e5ebaae8ad30b0c3f56562bb59b1",
+        "link": "https://open.spotify.com/artist/2krUNMgFZYm5s4Nn0g91W9"
     },
     {
         "name": "VINXEN",
@@ -46,11 +51,6 @@ export const topArtists = [
         "name": "Bryant Barnes",
         "image": "https://i.scdn.co/image/ab6761610000e5eb6a72681c50b8d528b586473a",
         "link": "https://open.spotify.com/artist/1URGXPcURc0aQzLeph65nK"
-    },
-    {
-        "name": "HAON",
-        "image": "https://i.scdn.co/image/ab6761610000e5ebaae8ad30b0c3f56562bb59b1",
-        "link": "https://open.spotify.com/artist/2krUNMgFZYm5s4Nn0g91W9"
     },
     {
         "name": "Sia",
