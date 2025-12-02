33ba8807bc2247b8150952eba7752d8abf34341c
biojameskim <biojameskim2002@gmail.com>
2025-12-01 21:12:39 -0600

    add april 2025 to gallery

--- src/data/gallery-folders.ts
+++ src/data/gallery-folders.ts
@@ -1398,5 +1398,398 @@ export const photoFolders: PhotoFolder[] = [
                 ]
             }
         ]
+    },
+    {
+        year: 2025,
+        month: "04",
+        photos: [
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642109/IMG_7889_l6yons.jpg",
+                caption: "grace's 22nd",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642082/IMG_5729_jnbuy2.jpg",
+                caption: "paul.",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642088/IMG_5733_ava6yn.jpg",
+                caption: "not sure what happened to jaden",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642078/IMG_5754_lbhp6u.jpg",
+                caption: "april bdays",
+                orientation: "h"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642084/IMG_5759_gvfklw.jpg",
+                caption: "er coffeehouse",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642079/IMG_5761_itrsh4.jpg",
+                caption: "derek can you put that down for a moment",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642120/IMG_5806_ed2hym.jpg",
+                caption: "spring",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642089/IMG_5807_pe3fev.jpg",
+                caption: "jiajun",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642091/IMG_5822_n42zfc.jpg",
+                caption: "",
+                orientation: "h"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642087/IMG_5825_c2c5rl.jpg",
+                caption: "during sunday service",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642118/IMG_5834_lkbawx.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642090/IMG_5838_mjajho.jpg",
+                caption: "ignite",
+                orientation: "h"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642092/IMG_5846_ckjave.jpg",
+                caption: "",
+                orientation: "h"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642108/IMG_5851_w5y2x8.jpg",
+                caption: "welcome to ithaca gary",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642093/IMG_5857_n4ndpk.jpg",
+                caption: 'gary asked "the halal brothers" if they were brothers',
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642080/IMG_5867_rsx8uh.jpg",
+                caption: "unideal sleeping arrangement",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642105/IMG_5876_l1v1vt.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642101/IMG_5877_gvpzmf.jpg",
+                caption: "burning calories",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642096/IMG_5891_gdoe2w.jpg",
+                caption: "...for ice cream",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642104/IMG_5894_idk1q1.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642119/IMG_5895_mpb9dz.jpg",
+                caption: "inner child",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642113/IMG_5898_udnwc2.jpg",
+                caption: "gary's last night",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642096/IMG_5916_mqqngs.jpg",
+                caption: "",
+                orientation: "h"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642100/IMG_5934_hyswmh.jpg",
+                caption: "cass's 22nd",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642106/IMG_5940_fbtkzb.jpg",
+                caption: "fav emcee",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642096/IMG_5942_byh2wu.jpg",
+                caption: "simon visits",
+                orientation: "h"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642101/IMG_5957_jmt5mj.jpg",
+                caption: "baptism sunday",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642113/IMG_5959_njrhhs.jpg",
+                caption: "er picnic",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642098/IMG_5966_p2ixl2.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642122/IMG_5967_ozmgsl.jpg",
+                caption: "still under construction",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642110/IMG_5968_iapseh.jpg",
+                caption: "eric visits",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642108/IMG_5974_qqad3q.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642081/IMG_6059_o3n6td.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642122/IMG_6071_dhgrf8.jpg",
+                caption: "",
+                orientation: "v"
+            }
+        ],
+        subfolders: [
+            {
+                name: "spring-break-2025.zip", photos: [
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642994/IMG_5351_f925ri.jpg",
+                        caption: "first stop",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642997/IMG_5354_wuyfka.jpg",
+                        caption: "uncle dan's home",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642972/IMG_0171_ggle60.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643027/IMG_5362_pvwzgk.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642999/IMG_5364_u15xid.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643030/IMG_5367_k5rnir.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642980/IMG_5406_nuad79.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642981/IMG_5408_badzaf.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642989/IMG_6284_tmdzpq.jpg",
+                        caption: "uncle dan",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642997/IMG_5375_telvrg.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643000/IMG_5379_ed7ix0.jpg",
+                        caption: "late night snack pt.1",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642992/IMG_8099_xgohdm.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642978/IMG_5385_otksbo.jpg",
+                        caption: "really didn't know derek liked aquariums this much",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642991/IMG_8096_nbw7f6.jpg",
+                        caption: "for the aesthetic",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643000/IMG_5390_xquh7f.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642979/IMG_5402_yubgl3.jpg",
+                        caption: "no caption needed",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642982/IMG_5413_di9nlr.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642973/IMG_0183_lig31e.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642972/IMG_0033_ecqeen.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643004/IMG_5428_iiaxwq.jpg",
+                        caption: "clippers v. magic",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643026/IMG_8130_tct2ne.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642971/IMG_0025_a23ece.jpg",
+                        caption: "late night snack pt.2",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643001/IMG_5432_fhimay.jpg",
+                        caption: "scary movie (the conjuring)",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642984/IMG_5433_zalhwc.jpg",
+                        caption: "they asked if they could sleep in the same room as us",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643011/IMG_5436_uf46oi.jpg",
+                        caption: "uncle dan's backyard",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643006/IMG_5439_sgrlqp.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643031/IMG_6737_cmo4jl.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643024/IMG_6763_evo09r.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642977/IMG_4295_w2sfnn.jpg",
+                        caption: "homemade",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643007/IMG_5450_er9nzu.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643008/IMG_5457_k8j7od.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643012/IMG_5462_ecn2sn.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642974/IMG_2831_qujztb.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643013/IMG_5468_e01vgb.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643014/IMG_5492_jv9j68.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643019/IMG_5505_iz3g8q.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643016/IMG_5512_dji9qt.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642986/IMG_5472_koznit.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642988/IMG_5492_1_mofzat.jpg",
+                        caption: "staged",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764642976/IMG_2954_vr1jz9.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643020/IMG_5549_ybyauz.jpg",
+                        caption: "",
+                        orientation: "h"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1764643022/IMG_5556_ruyljl.jpg",
+                        caption: "thanks florida",
+                        orientation: "v"
+                    }
+
+                ]
+            }
+        ]
     }
 ];
