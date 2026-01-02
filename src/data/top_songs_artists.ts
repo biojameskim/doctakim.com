ce24a4ec4b8e16a3edad1d13f8ba8afd1e07ce0e
biojameskim <biojameskim2002@gmail.com>
2026-01-02 10:38:35 -0800

    chore: update Spotify music data

--- src/data/top_songs_artists.ts
+++ src/data/top_songs_artists.ts
@@ -1,60 +1,60 @@
 export const topSongs = [
     {
-        "title": "The Only One",
-        "artist": "SOYOU",
-        "cover": "https://i.scdn.co/image/ab67616d0000b273b76962f92ed440093f7e35d9",
-        "link": "https://open.spotify.com/track/7mk8b7hcnfOu4aZy2gCdWl"
+        "title": "Cherish (My Love)",
+        "artist": "ILLIT",
+        "cover": "https://i.scdn.co/image/ab67616d0000b273be38c944cece32b9978c1b41",
+        "link": "https://open.spotify.com/track/21LtZQQMQYwVIxc9O9Ygq8"
     },
     {
-        "title": "December 25th",
-        "artist": "Charlie Puth",
-        "cover": "https://i.scdn.co/image/ab67616d0000b2739a44bc42f5e541fe97f92823",
-        "link": "https://open.spotify.com/track/1QNPTNaw67BanVtwXKIX1a"
+        "title": "Loop",
+        "artist": "hartts",
+        "cover": "https://i.scdn.co/image/ab67616d0000b2732abfbfef5c992734be426259",
+        "link": "https://open.spotify.com/track/6Icpi1qvKsbfHkYkStb3fc"
     },
     {
-        "title": "ON THE TABLE (feat. Sik-K)",
-        "artist": "DIMO REX, Sik-K",
-        "cover": "https://i.scdn.co/image/ab67616d0000b2731263be1df9bae463ffc08a31",
-        "link": "https://open.spotify.com/track/2ONgnWMIA0KRkyLX6sxkDj"
+        "title": "Sincere Lies (Feat. GARY)",
+        "artist": "BIG Naughty, GARY",
+        "cover": "https://i.scdn.co/image/ab67616d0000b27399f28cac91270c4d4cbb9e3d",
+        "link": "https://open.spotify.com/track/1NK9RWPSHo1sti3n5gSR7z"
     },
     {
-        "title": "すきっ！ - 超ver",
-        "artist": "Cho Tokimeki Sendenbu",
-        "cover": "https://i.scdn.co/image/ab67616d0000b273f7e91b039f9948fc31e42c70",
-        "link": "https://open.spotify.com/track/0SxNslJ4zylzWBQTX4rBIe"
+        "title": "To Me From Me (Prod. TABLO)",
+        "artist": "KiiiKiii",
+        "cover": "https://i.scdn.co/image/ab67616d0000b273b3f7b769136681e40dfa662c",
+        "link": "https://open.spotify.com/track/6wIMIRovtvzVPxIjq8KhA5"
     },
     {
-        "title": "Bathroom Floor",
-        "artist": "Isabel van Gelder",
-        "cover": "https://i.scdn.co/image/ab67616d0000b273d7debdd8a66b88e36739283c",
-        "link": "https://open.spotify.com/track/3QFFda4IODSKbT839NW6BR"
+        "title": "One late night in 1994",
+        "artist": "Gwyn Dorado",
+        "cover": "https://i.scdn.co/image/ab67616d0000b273ee1dc4ee4cac66b3a72e4249",
+        "link": "https://open.spotify.com/track/1lLJ3XCNr2fwc1DY4VaaZY"
     }
 ];
 
 export const topArtists = [
     {
-        "name": "Heize",
-        "image": "https://i.scdn.co/image/ab6761610000e5eb14df5a6a1377b0cd425a328c",
-        "link": "https://open.spotify.com/artist/5dCvSnVduaFleCnyy98JMo"
+        "name": "hartts",
+        "image": "https://i.scdn.co/image/ab6761610000e5ebfd7a4b6384024ed00f5d8fe6",
+        "link": "https://open.spotify.com/artist/0a1stdnZdUUGkudEf9iymv"
     },
     {
-        "name": "HAON",
-        "image": "https://i.scdn.co/image/ab6761610000e5ebaae8ad30b0c3f56562bb59b1",
-        "link": "https://open.spotify.com/artist/2krUNMgFZYm5s4Nn0g91W9"
+        "name": "TRADE L",
+        "image": "https://i.scdn.co/image/ab6761610000e5eb4b40223171c12650602fbdae",
+        "link": "https://open.spotify.com/artist/6b29EYSqnLhUyxC3yyNbyr"
     },
     {
-        "name": "VINXEN",
-        "image": "https://i.scdn.co/image/ab6761610000e5eb1249e0e4671a04aa0d786386",
-        "link": "https://open.spotify.com/artist/4T6xiCykTP9rCNaEPU7D4q"
+        "name": "ASH ISLAND",
+        "image": "https://i.scdn.co/image/ab6761610000e5eb3e4becdd5732e924b98d4d9a",
+        "link": "https://open.spotify.com/artist/7IEhlwWQA7pCkEvzwwHehE"
     },
     {
-        "name": "Bryant Barnes",
-        "image": "https://i.scdn.co/image/ab6761610000e5eb6a72681c50b8d528b586473a",
-        "link": "https://open.spotify.com/artist/1URGXPcURc0aQzLeph65nK"
+        "name": "Gwyn Dorado",
+        "image": "https://i.scdn.co/image/ab6761610000e5eb63cc9f35ee92890645548bb7",
+        "link": "https://open.spotify.com/artist/7pZ42lLRSlDnoPCfINXCn9"
     },
     {
-        "name": "Sia",
-        "image": "https://i.scdn.co/image/ab6761610000e5eb91c19888fe2fa3d5066199d1",
-        "link": "https://open.spotify.com/artist/5WUlDfRSoLAfcVSX1WnrxN"
+        "name": "ILLIT",
+        "image": "https://i.scdn.co/image/ab6761610000e5eb169a757a7d0ba53a33984416",
+        "link": "https://open.spotify.com/artist/36cgvBn0aadzOijnjjwqMN"
     }
 ];
