4fe0ccb7b94ec2ff2ea79227c533ee9a0f6121e7
biojameskim <biojameskim2002@gmail.com>
2026-01-04 17:04:27 -0800

    add 2025 01 02 03

--- src/data/gallery-folders.ts
+++ src/data/gallery-folders.ts
@@ -2104,5 +2104,360 @@ export const photoFolders: PhotoFolder[] = [
                 ]
             }
         ]
+    },
+    {
+        year: 2025,
+        month: "03",
+        photos: [
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572226/IMG_5127_kcjj8q.jpg",
+                caption: "cornell v. princeton",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572227/IMG_5188_pdfu8r.jpg",
+                caption: "spring sunshine",
+                orientation: "h"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572227/IMG_5207_o6engd.jpg",
+                caption: "scrubs",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572228/IMG_5226_tx90sg.jpg",
+                caption: "in the studio",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572224/IMG_5238_uoqdh1.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572224/IMG_5261_i8cqfc.jpg",
+                caption: "on my walk home",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572234/IMG_5266_rsjlxu.jpg",
+                caption: "cliffe",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572229/IMG_5276_xb2bvu.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572231/IMG_5318_ijfhpd.jpg",
+                caption: "dragon day",
+                orientation: "v"
+            }
+        ]
+    },
+    {
+        year: 2025,
+        month: "02",
+        photos: [
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572236/IMG_3449_nhytbh.jpg",
+                caption: "feb bdays",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572238/IMG_4885_bu68wa.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572238/IMG_4925_lqcp2g.jpg",
+                caption: "caleb",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572241/IMG_4934_zib6ko.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572240/IMG_4955_hgqgnv.jpg",
+                caption: "mag",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572239/IMG_4991_re2bbf.jpg",
+                caption: "trip to boston",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572243/IMG_4996_j6mafa.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572238/IMG_5001_pfvnyi.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572242/IMG_5016_tpxyug.jpg",
+                caption: "",
+                orientation: "h"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572236/IMG_5019_qk3x9s.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572245/IMG_5023_elnbb1.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572245/IMG_5026_zlyxjf.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572244/IMG_5041_r8yrh0.jpg",
+                caption: "future doctor",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572246/IMG_5057_adot1x.jpg",
+                caption: "vocal lessons",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572248/IMG_5065_uwm9e6.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {  
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572249/IMG_5070_lu4qce.jpg",
+                caption: "melody's 22nd",
+                orientation: "v"
+            }
+        ]
+    },
+    {
+        year: 2025,
+        month: "01",
+        photos: [
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572216/IMG_4291_qcvb5d.jpg",
+                caption: "reunion",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572215/IMG_4717_ly7pyy.jpg",
+                caption: "zarif's crib",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572217/IMG_4732_lbhskl.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572217/IMG_4733_azmxsv.jpg",
+                caption: "",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572217/IMG_4738_jdp1pv.jpg",
+                caption: "la philharmonic",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572218/IMG_4762_adctiq.jpg",
+                caption: "visit to king",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572221/IMG_4791_eee24o.jpg",
+                caption: "math major merch",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572216/IMG_4832_ziftgt.jpg",
+                caption: "a peek into derek's room",
+                orientation: "v"
+            },
+            {
+                filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572220/IMG_4857_zb33lx.jpg",
+                caption: "into the inferno",
+                orientation: "v"
+            }
+        ],
+        subfolders: [
+            {
+                name: "hawaii.zip",
+                photos: [
+                    {  
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572198/IMG_4307_howsmj.jpg",
+                        caption: "waikoloa",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572197/IMG_4346_gu2yqc.jpg",
+                        caption: "honaunau",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572199/IMG_4351_byiyke.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572200/IMG_4353_q3htjz.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572208/IMG_4359_b2902d.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572196/IMG_1442_uqkuqx.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572201/IMG_4376_uoocrg.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {  
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572200/IMG_4378_fr85uh.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572201/IMG_4393_efmjib.jpg",
+                        caption: "kilauea",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572206/IMG_4402_xjibkq.jpg",
+                        caption: "kealakomo",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572195/IMG_0186_iqfdt7.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572195/IMG_0222_jbwwvd.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572209/IMG_4408_hjl50k.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572196/IMG_0206_e50uvw.jpg",
+                        caption: "kona",
+                        orientation: "v"
+                    },
+                    {  
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572203/IMG_4468_egtvoa.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572203/IMG_4470_oxoev0.jpg",
+                        caption: "akaka falls",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572211/IMG_4474_uphvjf.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572213/IMG_4483_yk4dee.jpg",
+                        caption: "hilo",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572209/IMG_4486_nhztv1.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572197/IMG_4491_tbaj9s.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572197/IMG_4533_ppf6oh.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572198/IMG_4536_xrnrae.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572207/IMG_4557_gedmvn.jpg",
+                        caption: "punalu'u",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572196/IMG_3648_gzpvo6.jpg",
+                        caption: "coconut",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572205/IMG_4566_sot9bi.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572206/IMG_4586_unyy0u.jpg",
+                        caption: "polulu valley",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572207/IMG_4594_g0syuw.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572210/IMG_4601_biblct.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572204/IMG_4644_lgx7u1.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572196/IMG_1592_rn9rxx.jpg",
+                        caption: "kamakapa'a trail",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572206/IMG_4657_hqaj1s.jpg",
+                        caption: "",
+                        orientation: "v"
+                    },
+                    {
+                        filename: "https://res.cloudinary.com/doypjterz/image/upload/v1767572205/IMG_4696_kl15qy.jpg",
+                        caption: "",
+                        orientation: "v"
+                    }
+                ]
+            }
+        ]
     }
 ];
