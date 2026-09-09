af8589c970a2267410c87b3c063c326c212f83b0
James Kim <biojameskim2002@gmail.com>
2026-09-08 23:50:23 -0400

    Lazy mount blog tabs and preserve visited content

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -63,7 +63,16 @@ const Blog = () => {
         <Heading textAlign='center' fontSize={{ base: '2.2rem', md: '2.6rem' }} fontWeight={'medium'} pt={'8vh'} pb={{ base: '2vh', md: '4vh' }} >
           Blog
         </Heading>
-        <Tabs index={tabIndex} onChange={handleTabsChange} orientation='horizontal' variant='soft-rounded' colorScheme='green' align='center'>
+        <Tabs
+          index={tabIndex}
+          onChange={handleTabsChange}
+          orientation='horizontal'
+          variant='soft-rounded'
+          colorScheme='green'
+          align='center'
+          isLazy
+          lazyBehavior='keepMounted'
+        >
           <TabList paddingBottom='5vh' gap={{ base: '0.2rem', md: '0.5rem' }}>
             <Tab fontWeight={'normal'} fontSize={{ base: '0.9rem', md: '1rem' }} color={useColorModeValue('gray.600', 'white')} _active={{ bg: 'transparent' }} css={{ WebkitTapHighlightColor: "transparent", touchAction: "manipulation" }}>Stories</Tab>
             <Tab fontWeight={'normal'} fontSize={{ base: '0.9rem', md: '1rem' }} color={useColorModeValue('gray.600', 'white')} _active={{ bg: 'transparent' }} css={{ WebkitTapHighlightColor: "transparent", touchAction: "manipulation" }}>Thoughts</Tab>
